88b6c9cd2c9b60b35a523abe89e0e79347ac5ddf
Abhishek <abhishekonline45@gmail.com>
2024-05-07 18:50:43 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,68 @@
+# # print("Hey")
+# # print('abhishek')
+# a=20
+# b=30
+# print(a+b)
+
+# Arithmetic Opertors
+a=4
+b=5
+print("a==b:",a==b) #False
+print("a!=b:",a!=b) #True
+print("a>b:",a>b) #False
+print("a<b:",a<b) #True
+print("a>=b:",a>=b) #False
+print("a<=b:",a<=b) #True
+
+# Assignment Operators
+
+a=10
+a+=5
+# a-=5
+print("a+=5:",a)
+# print("a-=5:",a)
+
+# Bitwise Operators
+
+#Binary AND 
+a=60
+b=13
+c=a&b
+print("a&b:",c)
+
+
+#Binary OR
+
+a=60
+b=13
+c=a|b
+
+print("a|b:",c)
+
+
+#Binary XOR
+a=60
+b=13
+c=a^b
+print("a^b:",c)
+
+#BINARY ONES COMPLEMENT
+a=60
+b=13
+c=~a
+print("~a:",c)
+
+#Binary LEFT SHIFT
+a=60
+b=2
+c=a<<2
+print("a<<2:",c)
+
+
+#Binary RIGHT SHIFT
+a=60
+b=13
+c=a>>2
+print("a>>2:",c)
+
+

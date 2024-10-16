021132b1d4ddf55bdeb64be496166d6c2af507e3
Abhishek <abhishekonline45@gmail.com>
2024-10-16 14:58:09 +0530

    first commit

---
+++ Tasks Pkd/swap2variables.py
@@ -0,0 +1,11 @@
+a=int(input("Enter First Number : "))
+b=int(input("Enter Second Number : "))
+
+print("Before swapping a :",a)
+print("Before swapping b :",b)
+#logic to swap without using third variable 
+a=a+b 
+b
+
+print("After swapping a becomes :",a)
+print("After swapping b becomes :",b)
